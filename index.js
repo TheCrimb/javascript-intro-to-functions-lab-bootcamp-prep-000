db7b5179756e3ed2e550d38905ce6bd9c2807aff
Christine R Cooper <cooper.christine.r@gmail.com>
2019-03-05 04:51:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log('HELLO');
+  
+}
+function logWhisper(string) {
+  console.log('hello');
+}
+
+
+
+
+function sayHiToGrandma(string) {
+ 
+var lowercase = 'hello';
+var uppercase = 'HELLO!';
+
+if (lowercase.toLowerCase() === lowercase)
+  return "I can\'t hear you!";
+  
+}
+
+{
+var uppercase = 'HELLO!';
+ if (uppercase.toUpperCase() === uppercase) 
+ return "YES INDEED!";
+}
+
+
